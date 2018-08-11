9f9eb66df631746ea710f8cb124cdf57a08ad211
rich <rich@richs-Mac-Pro.local>
2018-08-11 12:17:49 -0700

    added new files

--- string_lenghthv1.py
+++ string_lenghthv1.py
@@ -1,6 +1,9 @@
+#! /usr/local/bin/python3
 attendees = ["ken", "Alena", "Treausre"]
 attendees.append("ashley")
 attendees.extend(["James", "Gill"])
 optional_invitees = ["Ben.J", "Dave"]
 potential_attendees = attendees + optional_invitees
+
+if (len(potential_attendees)
 print ("There are", len(potential_attendees), "potential attendees currently")
